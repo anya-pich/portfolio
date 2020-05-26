e5b1fff79030d2d7683fa0993f7069afb90476f0
anya-pich <anna.pich@outlook.com>
2020-05-25 21:33:43 -0700

    projects and articles landings created

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Anna Pichugina`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Anna Pichugina`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,6 +20,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/pages/projects.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { graphql } from "gatsby"
+// import { css } from "@emotion/core"
+// import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h1
+					style={{
+						display: `inline-block`,
+						borderBottom: `1px solid`,
+					}}
+        >
+          Amazing Pandas Eating Things
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+							style={{
+								marginBottom: "5px",
+							}}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                style={{color: `#bbb`}}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
